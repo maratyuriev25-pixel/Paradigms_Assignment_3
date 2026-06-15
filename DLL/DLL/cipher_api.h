db5bbf7077767c0aa4e7d66b9c0d4f900fe6ea28
maratyuriev25-pixel <marat.yuriev.25@kse.org.ua>
2026-06-15 14:36:47 +0300

    DLL Done

---
+++ DLL/DLL/cipher_api.h
@@ -0,0 +1,23 @@
+#pragma once
+typedef void cipher_t;
+
+#ifdef _WIN32
+#define EXPORT __declspec(dllexport)
+#else
+#define EXPORT __attribute__((visibility("default")))
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    EXPORT cipher_t* cipher_create_caesar(int key);
+    EXPORT cipher_t* cipher_create_vigenere(const char* key);
+    EXPORT char* cipher_encrypt(cipher_t* cipher, const char* text);
+    EXPORT char* cipher_decrypt(cipher_t* cipher, const char* text);
+    EXPORT void cipher_destroy(cipher_t* cipher);
+    EXPORT void cipher_free(char* str);
+
+#ifdef __cplusplus
+}
+#endif
